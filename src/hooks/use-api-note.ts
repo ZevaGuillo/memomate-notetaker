4cd321bc8d7503ef0ead3d4f576b25d5547d9528
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-24 22:30:39 -0500

    feat: Add Toast to notes hook

--- src/hooks/use-api-note.ts
+++ src/hooks/use-api-note.ts
@@ -1,10 +1,12 @@
 import { useSession } from "next-auth/react";
+import { useToast } from "~/components/ui/use-toast";
 import { useNoteStore } from "~/store/notetackerStore";
 import { type RouterOutputs, api } from "~/utils/api";
 
 type Topic = RouterOutputs["topic"]["getAll"][0];
 
 export const useApiNote = () => {
+  const { toast } = useToast()
 
     const { data: sessionData } = useSession();
   const { currentTopic } = useNoteStore();
@@ -22,18 +24,27 @@ export const useApiNote = () => {
   const createNote = api.note.create.useMutation({
     onSuccess: () => {
       void refetchNotes();
+      toast({
+        description: "Note successfully created!",
+      })
     },
   });
 
   const updateNote = api.note.update.useMutation({
     onSuccess: () => {
       void refetchNotes();
+      toast({
+        description: "Note successfully updated!",
+      })
     },
   });
 
   const deleteNote = api.note.delete.useMutation({
     onSuccess: () => {
       void refetchNotes();
+      toast({
+        description: "Note successfully deleted!",
+      })
     },
   });
 
