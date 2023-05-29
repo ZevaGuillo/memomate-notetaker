56819ba31fbd7ffed0f120a44dcc16fb8afbf79f
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-29 13:09:34 -0500

    feat: Add avatar popover to header

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getInitials(username:string) {
+  const names = username.split(' ');
+
+  // Obtener la primera letra de cada nombre y unirlas
+  const initials = names.map(name => name[0]).join('');
+
+  return initials;
 }
